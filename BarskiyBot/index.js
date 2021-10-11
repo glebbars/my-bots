0707a4b662f162d3bdb97a7010f6ece3f238b302
Gleb <glebars2004@gmail.com>
2021-10-12 01:18:00 +0300

    added reply/delete/edit/forward functions

--- BarskiyBot/index.js
+++ BarskiyBot/index.js
@@ -40,40 +40,176 @@ const bot = new TelegramBot(token, {
 /// LINK AND NOTIFICATIONS SETTINGS
 
 
+/// SIMPLE KEYBOARD
 
-bot.on('message', (msg) => {
+// bot.on('message', (msg) => {
 
-  if (msg.text === 'Close') {
-    bot.sendMessage(msg.chat.id, 'Close', {
+//   if (msg.text === 'Close') {
+//     bot.sendMessage(msg.chat.id, 'Close', {
+//       reply_markup: {
+//         remove_keyboard: true
+//       }
+//     })
+//   }
+//   else if (msg.text === 'Reply') {
+//     bot.sendMessage(msg.chat.id, 'ok', {
+//       reply_markup: {
+//         force_reply: true
+//       }
+//     })
+//   } else {
+//     bot.sendMessage(msg.chat.id, 'keyboard', {
+//       reply_markup: {
+//         keyboard: [
+//           [{
+//             text: 'Share my geolocation',
+//             request_location: true
+//           }],
+//           ['Close', 'Reply'],
+//           [{
+//             text: 'Share my contact',
+//             request_contact: true
+//           }],
+//         ],
+//         one_time_keyboard: true
+//       }
+//     })
+//   }
+// }) 
+
+
+/// SIMPLE KEYBOARD
+
+
+/// INLINE KEYBOARD
+
+//  bot.on('message', msg => {
+//    const id = msg.chat.id
+
+//   bot.sendMessage(id, 'Inline keyboard', {
+//     reply_markup: {
+//       inline_keyboard: [
+//         [
+//           { 
+//             text: 'Google',
+//             url: 'https://www.google.com',
+//             callback_data: '1'
+//           }
+//         ],
+//         [
+//           {
+//             text: 'Reply',
+//             callback_data: 'reply' 
+//           },
+//           {
+//             text: 'Forward',
+//             callback_data: 'forward'
+//           }
+//         ]
+//       ]
+//     }
+//   })
+// })
+
+
+/// SHARE CONTENT FROM YOUR BOT (GUF, VIDEO, ARTICLE, etc.)
+
+// bot.on('callback_query', query => {
+//   bot.answerCallbackQuery(query.id, `${query.data}`)
+// })
+
+// bot.on('inline_query', query => {
+
+//   const results = []
+
+//   for(let i =0; i< 5; i++){
+//     results.push({
+//       type: 'article',
+//       id: i.toString(),
+//       title: `Title ${i}`,
+//       input_message_content: {
+//         message_text: `Article #${i+1}`
+//       }
+
+       
+//     })
+//   }
+
+//   bot.answerInlineQuery(query.id, results, {
+//     cache_time: 0
+//   })
+// })
+
+/// SHARE CONTENT FROM YOUR BOT (GUF, VIDEO, ARTICLE, etc.)
+
+
+const inlineKeyboard = [
+  [
+    {
+      text: 'Forward',
+      callback_data: 'forward'
+    },
+    {
+      text: 'Reply',
+      callback_data: 'reply'
+    }
+  ],
+  [
+    {
+      text: 'Edit',
+      callback_data: 'edit'
+    },
+    {
+      text: 'Delete',
+      callback_data: 'delete'
+    }
+  ]
+]
+
+bot.onText(/\/start/, (msg, [source, match]) => {
+  const chatID = msg.chat.id
+
+  bot.sendMessage(chatID, 'Keyboard', {
+    reply_markup: {
+      inline_keyboard: inlineKeyboard
+    }
+  })
+
+  // bot.on('callback_query', query => {
+  //   bot.answerCallbackQuery(query.id, `${query.data}`)
+  //   bot.sendMessage(chatID, query.data)
+  // })
+})
+
+bot.on('callback_query', query => {
+
+   const {chat, message_id, text} = query.message
+
+  switch(query.data){
+    case 'forward':
+      // where (chat id), from which chat, what do you send
+      bot.forwardMessage(chat.id, chat.id, message_id )
+    break;
+    case 'reply':
+    bot.sendMessage(chat.id, 'Answering the message', {
+      reply_to_message_id: message_id,
       reply_markup: {
-        remove_keyboard: true
+        inline_keyboard: inlineKeyboard
       }
     })
+    break;
+    case 'edit':
+      bot.editMessageText(`${text} edited`, {
+        chat_id: chat.id,
+        message_id: message_id,
+        reply_markup: {inline_keyboard: inlineKeyboard}
+      })
+      break;
+      case 'delete': 
+      bot.deleteMessage(chat.id, message_id)
   }
-  else if (msg.text === 'Reply') {
-    bot.sendMessage(msg.chat.id, 'ok', {
-      reply_markup: {
-        force_reply: true
-      }
-    })
-  } else {
-    bot.sendMessage(msg.chat.id, 'keyboard', {
-      reply_markup: {
-        keyboard: [
-          [{
-            text: 'Share my geolocation',
-            request_location: true
-          }],
-          ['Close', 'Reply'],
-          [{
-            text: 'Share my contact',
-            request_contact: true
-          }],
-        ],
-        one_time_keyboard: true
-      }
-    })
-  }
 
-console.log('***')
+    bot.answerCallbackQuery({
+      callback_query_id: query.id
+    })
 })
